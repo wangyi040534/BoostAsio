07457aeba3a2cede837627d81c9a14161aff585b
wangyi040534 <wy040534@163.com>
2015-04-07 16:20:43 +0800

    boost asio tutorial

--- test.cpp
+++ test.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
-
+void print(const boost::system::error_code)
+{
+  std::cout << "hello 3" << std::endl;
+  }
 int main()
 {
 boost::asio::io_service io;
 boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
-t.wait();
-std::cout<<"hello"<<std::endl;
+//t.wait();
+t.async_wait(&print);
+std::cout << "hello 1" << std::endl;
+io.run();
+std::cout << "hello 2" << std::endl;
+//std::cout<<"hello"<<std::endl;
 return 0;
 }
